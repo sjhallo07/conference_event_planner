b3d4e97718e3c5b40d6d266390d37c81b80d2b93
Marcos Mora <abreu760@hotmail.com>
2025-07-29 17:07:05 -0400

    finish fixes

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,22 +1,61 @@
+//avSlice.js
+
 import { createSlice } from "@reduxjs/toolkit";
 
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "https://cdn.pixabay.com/photo/2012/03/01/01/10/business-20031_1280.jpg",
+        name: "Proyectores",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2019/04/07/09/41/speakers-4109274_1280.jpg",
+        name: "Altavoz",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2019/01/11/01/54/public-speaking-3926344_1280.jpg",
+        name: "Micrófonos",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2015/01/08/18/30/white-board-593370_1280.jpg",
+        name: "Pizarras",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: "https://cdn.pixabay.com/photo/2014/06/18/20/58/directory-371692_640.jpg",
+        name: "Señalización",
+        cost: 80,
+        quantity: 0,
+    },
+ 
   ],
 
 
-  reducers: {
+   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
-    decrementAvQuantity: (state, action) => {
-     
+
+
+  decrementAvQuantity: (state, action) => {
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
-
-export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
+export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions
 
 export default avSlice.reducer;
--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,17 +1,18 @@
 // mealsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
-
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Desayuno', cost: 50, selected: false },
+    { name: 'Té de la tarde', cost: 25, selected: false },
+    { name: 'Almuerzo', cost: 65, selected: false },
+    { name: 'Cena', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload].selected;
     },
   },
 });
-
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
